4cd7dc8bc4a9b7998e9c3b903309ece83482dc3c
rludwik <robertludwik96@gmail.com>
2022-12-12 08:00:00 -0500

    add to cart DL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import { Outlet, Link } from "react-router-dom";
 
+
+const addToCart = () => {
+  window.dataLayer.push({
+    'event': 'add to cart',
+    'items': 1
+   });
+}
+
 function App() {
   return (
     <div>
@@ -17,6 +25,7 @@ function App() {
         <Link to="/subscribe">Subscribe</Link>
       </nav>
       <Outlet />
+      <button onClick={addToCart()}>Add to Cart</button>
     </div>
   );
 }
